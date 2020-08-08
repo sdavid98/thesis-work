670231ed61ae28587a6811524e32249331b4d571
Sagi David <sagidavid98@gmailcom>
2020-08-08 15:07:28 +0200

    experimental lib no2

--- src/SandBox.js
+++ src/SandBox.js
@@ -1,248 +1,180 @@
-/*
-import React, {Component, useState} from 'react';
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
-
-
-const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-
-    return result;
-};
-
-const reorderQuoteMap = ({quoteMap, source, destination,}) => {
-    const current = [...quoteMap[source.droppableId]];
-    const next = [...quoteMap[destination.droppableId]];
-    const target = current[source.index];
-
-    // moving to same list
-    if (source.droppableId === destination.droppableId) {
-        const reordered = reorder(
-            current,
-            source.index,
-            destination.index,
-        );
-        const result = {
-            ...quoteMap,
-            [source.droppableId]: reordered,
-        };
-        return {
-            quoteMap: result,
-        };
-    }
-
-    // moving to different list
-
-    // remove from original
-    current.splice(source.index, 1);
-    // insert into next
-    next.splice(destination.index, 0, target);
-
-    const result = {
-        ...quoteMap,
-        [source.droppableId]: current,
-        [destination.droppableId]: next,
-    };
-
-    return {
-        quoteMap: result,
-    };
-};
-
-const Column = props => {
-    const title = props.title;
-    const quotes = props.quotes;
-    const index = props.index;
-
-    return (
-        <Draggable draggableId={title} index={index}>
-            {(provided, snapshot) => (
-                <div ref={provided.innerRef} {...provided.draggableProps}>
-                    <div>
-                        <div
-                            {...provided.dragHandleProps}
-                            aria-label={`${title} quote list`}
-                        >
-                            {title}
-                        </div>
-                    </div>
-                    <Droppable droppableId={`${index}`}>
-                        {(provided) => (
-                            <div
-                                ref={provided.innerRef}
-                                style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)'}}
-                            >
-                                {quotes.map((item, ind) => (
-                                    <Draggable
-                                        key={item.id}
-                                        draggableId={item.id}
-                                        index={ind}
-                                    >
-                                        {provided => (
-                                            <div
-                                                ref={provided.innerRef}
-                                                {...provided.draggableProps}
-                                                {...provided.dragHandleProps}
-                                                style={{border: '1px solid blue', padding: '15px'}}
-                                            >
-                                                {item.content}
-                                            </div>
-                                        )}
-                                    </Draggable>
-                                ))}
-                            </div>
-                        )}
-                    </Droppable>
-                </div>
-            )}
-        </Draggable>
-    );
-
-};
-
-const data = [
-
-        [
-            {
-                id: '10',
-                content: 'I should not have drunk that much tea!',
-                author: 'princess',
-            },
-            {
-                id: '11',
-                content: 'Please! I need the real you!',
-                author: 'princess',
-            },
-            {
-                id: '12',
-                content: "Haven't slept for a solid 83 hours, but, yeah, I'm good.",
-                author: 'princess',
-            },
-        ],
-        [
-            {
-                id: '13',
-                content: 'have drunk that much tea!',
-                author: 'princess',
-            },
-            {
-                id: '14',
-                content: 'Please!the real you!',
-                author: 'princess',
-            },
-            {
-                id: '15',
-                content: "Haven'id 83 hours, but, yeah, I'm good.",
-                author: 'princess',
-            },
-        ],
-        [
-            {
-                id: '16',
-                content: 'I should that much tea!',
-                author: 'princess',
-            },
-            {
-                id: '17',
-                content: 'Please! I need ou!',
-                author: 'princess',
-            },
-            {
-                id: '18',
-                content: "3 hours, but, yeah, I'm good.",
-                author: 'princess',
-            },
-        ]
-];
-
-const SandBox = () => {
-    const [state, setState] = useState({
-        columns: data,
-        ordered: Object.keys(data)
-    });
-
-    const onDragEnd = (result) => {
-        console.log('DRAG END');
-        // dropped nowhere
-        if (!result.destination) {
-            return;
-        }
-
-        const source = result.source;
-        const destination = result.destination;
-
-        // did not move anywhere - can bail early
-        if (
-            source.droppableId === destination.droppableId &&
-            source.index === destination.index
-        ) {
-            return;
-        }
-
-        // reordering column
-        if (result.type === 'COLUMN') {
-            const ordered = reorder(
-                state.ordered,
-                source.index,
-                destination.index,
-            );
-
-            setState({
-                ordered: ordered,
-                columns: state.columns
-            });
-
-            return;
-        }
-
-        const data = reorderQuoteMap({
-            quoteMap: state.columns,
-            source,
-            destination,
-        });
-
-        setState({
-            ordered: state.ordered,
-            columns: data.quoteMap,
-        });
-    };
-
-    const board = (
-        <Droppable
-            droppableId="board"
-            type="COLUMN"
-            direction="horizontal"
-            ignoreContainerClipping={true}
-            isCombineEnabled={false}
-        >
-            {(provided) => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                    {state.ordered.map((key, index) => (
-                        <Column
-                            key={key}
-                            index={index}
-                            title={key}
-                            quotes={state.columns[key]}
-                            isCombineEnabled={false}
-                        />
-                    ))}
-                    {provided.placeholder}
-                </div>
-            )}
-        </Droppable>
-    );
-
-    return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            {board}
-        </DragDropContext>
-    );
-};*/
-
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+
+const rows = [
+    {
+        id: 'row1',
+        type: 'main',
+        children: ['col1', 'col2'],
+        contentId: false
+    },
+    {
+        id: 'row2',
+        type: 'main',
+        children: ['col3', 'col4', 'col5'],
+        contentId: false
+    },
+    {
+        id: 'row3',
+        type: 'inner',
+        children: [],
+        contentId: 'con1'
+    },
+    {
+        id: 'row4',
+        type: 'inner',
+        children: [],
+        contentId: 'con2'
+    },
+    {
+        id: 'row5',
+        type: 'inner',
+        children: [],
+        contentId: 'con3'
+    },
+    {
+        id: 'row6',
+        children: [],
+        contentId: 'con4'
+    },
+    {
+        id: 'row7',
+        type: 'inner',
+        children: [],
+        contentId: 'con5'
+    },
+    {
+        id: 'row8',
+        type: 'inner',
+        children: [],
+        contentId: 'con6'
+    },
+    {
+        id: 'row9',
+        type: 'inner',
+        children: [],
+        contentId: 'con7'
+    }
+];
+
+const colums = [
+    {
+        id: 'col1',
+        width: '40%',
+        children: ['row3', 'row4']
+    },
+    {
+        id: 'col2',
+        width: '60%',
+        children: ['row5']
+    },
+    {
+        id: 'col3',
+        width: '30%',
+        children: ['row6']
+    },
+    {
+        id: 'col4',
+        width: '40%',
+        children: ['row7', 'row8']
+    },
+    {
+        id: 'col5',
+        width: '30%',
+        children: ['row9']
+    }
+];
+
+const content = [
+    {
+        id: 'con1',
+        content: 'Content1'
+    },
+    {
+        id: 'con2',
+        content: 'Content2'
+    },
+    {
+        id: 'con3',
+        content: 'Content3'
+    },
+    {
+        id: 'con4',
+        content: 'Content4'
+    },
+    {
+        id: 'con5',
+        content: 'Content5'
+    },
+    {
+        id: 'con6',
+        content: 'Content6'
+    },
+    {
+        id: 'con7',
+        content: 'Content7'
+    }
+];
+
+const data = [
+    {
+        id: 'row1',
+        children: [
+            {
+                id: 'col1',
+                width: '40%',
+
+            }
+        ]
+    }
+];
+
+const renderItem = (item, index) => (
+    <Draggable key={index} draggableId={item.id} index={index}>
+        {(provided, snapshot) => (
+            <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+            >{item.content}</div>
+        )}
+    </Draggable>
+);
+
+
+const renderDnD = (renderDndLowerLevel, row, index) => (
+    <Droppable droppableId={`${index}`} type={row.type} key={row.id}>
+        {(dropProvided, dropSnapshot) => (
+            <div
+                ref={dropProvided.innerRef}
+                {...dropProvided.droppableProps}
+                style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)" }}
+            >
+
+                {row.children.length > 0 ? colums.filter(col => row.children.indexOf(col.id) >= 0).map((col, index) => (
+                    <Draggable draggableId={col.id} key={col.id} index={index}>
+                        {(dragProvided, dragSnapshot) => (
+                            <div
+                                ref={dragProvided.innerRef}
+                                {...dragProvided.draggableProps}
+                                {...dragProvided.dragHandleProps}
+                            >{console.log(rows.filter(row => col.children.indexOf(row.id) >= 0))}
+                                {rows.filter(row => col.children.indexOf(row.id) >= 0).map(
+                                    (innerRow, index) => innerRow.children.length > 0 ? renderDndLowerLevel(renderDndLowerLevel, innerRow, index) : renderItem(content.find(con => con.id === innerRow.contentId), index)
+                                )}
+                            </div>
+                        )}
+                    </Draggable>
+                )) : renderItem(content.find(con => con.id === row.contentId), index)}
+                {dropProvided.placeholder}
+            </div>
+        )}
+    </Droppable>
+);
+
+
 
 // fake data generator
 const getItems = (count, offset = 0) =>
@@ -263,6 +195,7 @@ const reorder = (list, startIndex, endIndex) => {
  * Moves an item from one list to another list.
  */
 const move = (source, destination, droppableSource, droppableDestination) => {
+    console.log(source, destination, droppableSource, droppableDestination);
     const sourceClone = Array.from(source);
     const destClone = Array.from(destination);
     const [removed] = sourceClone.splice(droppableSource.index, 1);
@@ -295,7 +228,7 @@ const getListStyle = isDraggingOver => ({
 });
 
 const SandBox = () => {
-    const [state, setState] = useState([getItems(10), getItems(5, 10)]);
+    const [state, setState] = useState([getItems(3), getItems(2, 3)]);
 
     function onDragEnd(result) {
         console.log(result);
@@ -307,13 +240,16 @@ const SandBox = () => {
         }
         const sInd = +source.droppableId;
         const dInd = +destination.droppableId;
+        console.log(sInd, dInd);
 
         if (sInd === dInd) {
+            console.log('REORDER');
             const items = reorder(state[sInd], source.index, destination.index);
             const newState = [...state];
             newState[sInd] = items;
             setState(newState);
         } else {
+            console.log('MOVE');
             const result = move(state[sInd], state[dInd], source, destination);
             const newState = [...state];
             newState[sInd] = result[sInd];
@@ -336,7 +272,33 @@ const SandBox = () => {
             <button
                 type="button"
                 onClick={() => {
-                    setState([...state, getItems(1)]);
+                    setState([[...state[0], ...getItems(1)], ...state.slice(1)]);
+                }}
+            >
+                Add new item
+            </button>
+            <div>
+                <DragDropContext onDragEnd={onDragEnd}>
+                    {rows.filter(row => row.type === 'main').map((row, index) => renderDnD(renderDnD, row, index))}
+                </DragDropContext>
+            </div>
+        </div>
+    );
+
+    return (
+        <div>
+            <button
+                type="button"
+                onClick={() => {
+                    setState([...state, []]);
+                }}
+            >
+                Add new group
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    setState([[...state[0], ...getItems(1)], ...state.slice(1)]);
                 }}
             >
                 Add new item
@@ -351,6 +313,7 @@ const SandBox = () => {
                                     style={getListStyle(snapshot.isDraggingOver)}
                                     {...provided.droppableProps}
                                 >
+                                    {console.log(state, ind, el)}
                                     {el.map((item, index) => (
                                         <Draggable
                                             key={item.id}
