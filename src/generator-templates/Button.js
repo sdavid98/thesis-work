ff3d54ef0369ba5a482a5eb651e6c0ab7145a070
Sagi David <sagidavid98@gmailcom>
2020-08-29 21:13:52 +0200

    started bulletproof button

--- src/generator-templates/Button.js
+++ src/generator-templates/Button.js
@@ -27,6 +27,18 @@ const button = item => {
         return span.outerHTML;
     }).join('');
 
+    const bulletProofButton = `
+    <div>
+        <!--[if mso]>
+        <v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="${item.content.link}" style="height:40px;v-text-anchor:middle;width:200px;" arcsize="10%" stroke="f" fillcolor="#ffffff">
+            <w:anchorlock/>
+            <center style="color:#ffffff;font-family:sans-serif;font-size:13px;font-weight:bold;">Show me the button!</center>
+        </v:roundrect>
+        <![endif]-->
+        <a href="https://" style="background-color:#556270;background-image:url(https://i.imgur.com/0xPEf.gif);border:1px solid #1e3650;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:13px;font-weight:bold;line-height:40px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">Show me the button!</a>
+    </div>
+    `;
+
     return pushStyleOnElement(a, {...removeUnusedStyles({...item.rootElementStyle}), ...additionalStyles});
 };
 
